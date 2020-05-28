3f7510b6755d1d7134e0cb35e518172a0e77475d
ditasmer <ditasmer@gmail.com>
2020-05-28 18:35:32 +0200

    swiper horizontal, buttons swip

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-//swiper script
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+//Javascript document
+
+var swiper;
+swiper = new Swiper('.swiper-container', {
+  slidesPerView: 4.2,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
