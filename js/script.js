f7f92a02524aaef8d7f93097fc75fa6c888b2423
ditasmer <ditasmer@gmail.com>
2020-05-30 21:22:47 +0200

    files position

--- js/script.js
+++ js/script.js
@@ -13,3 +13,14 @@ swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+/*script responsive menu*/
+
+    function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "nav-main") {
+        x.className += " responsive";
+      } else {
+        x.className = "nav-main";
+      }
+    }
